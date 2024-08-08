1c1fd6b617b19af79edc8c4257f03728815bc00f
Dilshod <qurbonmurotovd@gmail.com>
2024-08-08 10:49:40 +0500

    updatefile

--- todo_app/models/base.py
+++ todo_app/models/base.py
@@ -9,6 +9,6 @@ Base = declarative_base()
 class AbstractBase(Base):
     __abstract__ = True
 
-    id: Mapped[str] = mapped_column(types.String(), primary_key=True)
+    id: Mapped[str] = mapped_column(types.String(26), primary_key=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=datetime.now)
