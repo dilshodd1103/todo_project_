28ea2603b578911c538384c2948854e53970b315
Dilshod <qurbonmurotovd@gmail.com>
2024-11-04 14:24:45 +0500

    add CORS

--- todo_app/main.py
+++ todo_app/main.py
@@ -1,6 +1,8 @@
 import importlib
 import pkgutil
 from types import ModuleType
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from fastapi import FastAPI
 
@@ -15,6 +17,18 @@ def create_app() -> FastAPI:
     app = FastAPI()
     app.container = container
 
+    origins = [
+        "http://localhost",
+        "http://localhost:8080",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     include_routers(app, routers)
 
     return app
